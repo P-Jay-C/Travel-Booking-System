f69f7c30be75f92caf23c45e1081aa9ca39a6aff
Jeremiah Azilaku <jeremiah.azilaku@amalitech.com>
2024-02-12 09:18:10 +0000

    Created a setup file for the user-service, booking-service, category-service, api-gateway, configuration-server and discovery-server

---
+++ category-service/src/test/java/com/amalitech/org/categoryservice/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.org.categoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
