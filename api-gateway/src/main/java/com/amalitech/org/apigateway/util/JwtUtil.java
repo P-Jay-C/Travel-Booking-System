6acf39dd9ab0f4079b5293992985f2fe40c58be2
Jeremiah Azilaku <jeremiah.azilaku@amalitech.com>
2024-02-14 14:40:41 +0000

    Secured app secrets

--- api-gateway/src/main/java/com/amalitech/org/apigateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/amalitech/org/apigateway/util/JwtUtil.java
@@ -5,18 +5,17 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Component
 public class JwtUtil {
 
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    @Value("${ENCRYPTION_KEY}")
+    private String SECRET;
 
     public void validateToken(final String token) {
         try {
--- user-service/src/main/java/com/amalitech/org/userservice/service/JwtService.java
+++ user-service/src/main/java/com/amalitech/org/userservice/service/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -15,7 +16,8 @@ import java.util.Map;
 public class JwtService {
 
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    @Value("${ENCRYPTION_KEY}")
+    private String SECRET;
 
     public void validateToken(final String token) {
         try {
